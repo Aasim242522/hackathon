d7f5c1e1328b72150a8fac663996ff7c28cd05c2
Aasim Adamji <adamjimohammadaasimcs242522@gmail.com>
2026-08-28 11:35:35 +0530

    initial setup

---
+++ index.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>Hello World!</h1>"
+# --- Simple homepage / campus index ---
+@app.route("/aasim")
+def index1():
+    return "<h1>Hello Aasim</h1>"
+# --- Simple homepage / campus index ---
+@app.route("/ashif")
+def index2():
+    return "<h1>Hello Ashif!</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
